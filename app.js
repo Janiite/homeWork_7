2a6908a544bbb02a17981fc98b12244519d56f49
Jana Fedotovska <69121010+Janiite@users.noreply.github.com>
2021-05-26 17:51:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-const goBtn = ()=> {
-    const btn = document.getElementById('go');
-    
-    btn.addEventListener('click', ntexst = ()=>{
-          const fname = document.getElementById('fname').value;
-          const par = document.createElement('P');
-          document.body.appendChild(par);
-          if(fname === ''){
-            par.innerHTML = `Labdien svešineik! Kā tevi sauc?`;
-          }else {
-              par.innerHTML = `Labdien ${fname}! Prieks iepazīties,  mani sauc Botiņš!`;
-          }  
-    });
-}
+const goBtn = () => {
+  const btn = document.getElementById('go');
+  const answer = document.getElementById('answer');
 
+  btn.addEventListener('click', ntexst = () => {
+    const fname = document.getElementById('fname').value;
+    const par = document.createElement('P');
+    document.getElementById('answer').appendChild(par);
+    /*const del = document.createElement('BUTTON');
+    document.getElementById('answer').appendChild(del);
+    del.innerHTML = 'Dzēst';*/
+    const male = fname.charAt(fname.length - 1)
+    if (fname === '') {
+      par.innerHTML = `Labdien svešineik! Kā tevi sauc?`;
+    } else if (male === 's') {
+      par.innerHTML = `Labdien ${fname.slice(0, fname.length - 1)}! Prieks iepazīties,  mani sauc Botiņš!`;
+    } else {
+      par.innerHTML = `Labdien ${fname}! Prieks iepazīties,  mani sauc Botiņš!`;
+    }
+  });
+}
 goBtn();
 
 
